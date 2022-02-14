61dcfc2049f20b738dca4c451dd896955aa7f5ae
Lewis Heath <email@lewisheath.co.uk>
2022-02-14 23:23:18 +0000

    started on header and hero

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header-container">
+            <div className="header-title">
+                <h1>logo//</h1>
+            </div>
+            <div className="links">
+                <p>home()</p>
+                <p>about()</p>
+                <p>projects()</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero'>
+        <div className='hero-container'>
+            <div className='hero-title'>
+                <h1>lewis heath</h1>
+            </div>
+            <div className='hero-description'>
+                <p>Student at lancaster university studying computer science</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
